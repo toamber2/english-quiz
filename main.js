e7935673d5817fb377475487a4345704edb6bd49
Amber Elaine Touch <virtualassistant79@gmail.com>
2019-07-13 07:46:44 +0200

    show questions function complete

--- main.js
+++ main.js
@@ -7,6 +7,39 @@ function generateQuiz(questions, quizContainer, resultsContainer,submitButton){
 
     
      showQuestions(questions, quizContainer);
+     // we'll need a place to store the output and the answer choices
+	var output = [];
+	var answers;
+
+	// for each question...
+	for(var i=0; i<questions.length; i++){
+		
+		// first reset the list of answers
+		answers = [];
+
+		// for each available answer to this question...
+		for(letter in questions[i].answers){
+
+			// ...add an html radio button
+			answers.push(
+				'<label>'
+					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+					+ letter + ': '
+					+ questions[i].answers[letter]
+				+ '</label>'
+			);
+		}
+
+		// add this question and its answers to the output
+		output.push(
+			'<div class="question">' + questions[i].question + '</div>'
+			+ '<div class="answers">' + answers.join('') + '</div>'
+		);
+	}
+
+	// finally combine our output list into one string of html and put it on the page
+	quizContainer.innerHTML = output.join('');
+}
 
     
     submitButton.onclick = function(){
